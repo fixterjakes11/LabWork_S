958e94bd2af79a6a849768afe304e46df6602f0b
fixterjakes11 <rahmetovcom06@gmail.com>
2022-02-20 19:45:51 +0300

    Добавьте файлы проекта.

---
+++ LabWork/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LabWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вывод цифр от натурального числа.\nНажмите Enter...");
+            while (true)
+            {
+                if (Console.ReadKey().Key == ConsoleKey.Enter)
+                {
+                    try
+                    {
+                        Console.Write("Введите число: ");
+                        int x = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"Результат: {count(x)} ");
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Введите число");
+                    }
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+        }
+
+        static string count(int n)
+        {
+            
+            if (n > 10)
+            {
+                return $"{count(n / 10)} { n % 10 }";
+            }
+            return $"{n}";
+
+        }
+
+    }
+}
